d95594f1690926f821e9c3272ea0b0c2869ac56d
ReDesigned <arfoneajp@gmail.com>
2022-08-04 00:15:02 +0900

    add entityframework

---
+++ Shared/Database/Models/RiceDbContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Shared.Database.Models
+{
+    public partial class RiceDbContext : DbContext
+    {
+        public RiceDbContext()
+        {
+        }
+
+        public RiceDbContext(DbContextOptions<RiceDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=127.0.0.1;database=rstat;user id=rice;password=rice", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.6.0-mariadb"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_general_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.Id)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(100)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Password)
+                    .HasColumnType("text")
+                    .HasColumnName("password");
+
+                entity.Property(e => e.RegisteredAt)
+                    .HasColumnType("datetime")
+                    .HasColumnName("registered_at")
+                    .HasDefaultValueSql("current_timestamp()");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
