baf4e63772f0823bca150857bbeab04332f7dfed
SeojunYun <arfoneajp@gmail.com>
2022-12-22 11:52:57 +0900

    learning razor! add(BackendService): add testing status service

---
+++ AdministratorConsole/Data/BackendService.cs
@@ -0,0 +1,22 @@
+﻿using RestSharp;
+
+namespace AdministratorConsole.Data
+{
+    public class BackendService
+    {
+        private readonly string _api;
+        private readonly RestClient _client;
+        public BackendService(string api = "http://localhost:5000")
+        {
+            _api = api;
+            _client = new(_api);
+        }
+
+        public async Task<bool> IsServerAvailable()
+        {
+            RestRequest req = new("/status");
+            RestResponse res = await _client.ExecuteAsync(req);
+            return res.IsSuccessful;
+        }
+    }
+}
--- AdministratorConsole/Program.cs
+++ AdministratorConsole/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<WeatherForecastService>().AddSingleton<BackendService>();
 
 var app = builder.Build();
 
